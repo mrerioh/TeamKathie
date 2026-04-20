eb01c929839c463a27b3f2c209e3d1f0e8f5ff43
Erio <erio.hoti@torontomu.ca>
2026-04-20 19:37:10 -0400

    fix spacing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,56 +6,36 @@ public class PlayerMovement : MonoBehaviour
 
     public InputActionAsset InputActions;
 
-    private InputAction moveAction;
-    private InputAction jumpAction;
+    private InputAction     moveAction;
+    private InputAction     jumpAction;
 
-    private Vector2     moveAmt;
-    private Rigidbody2D rb;
+    private Vector2         moveAmt;
+    private Rigidbody2D     rb;
 
-    public float        walkSpeed = 5;
-    public float        jumpSpeed = 5;
+    public float            walkSpeed = 5;
+    public float            jumpSpeed = 5;
 
     private void OnEnable()
     {
-        InputActions.FindActionMap("Player").Enable();
+        InputActions.FindActionMap( "Player" ).Enable();
     }
 
     private void OnDisable()
     {
-        InputActions.FindActionMap("Player").Disable();
+        InputActions.FindActionMap( "Player" ).Disable();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        moveAction = InputSystem.actions.FindAction("Move");
-        jumpAction = InputSystem.actions.FindAction("Jump");
-        rb = GetComponent<Rigidbody2D>();
+        moveAction = InputSystem.actions.FindAction( "Move" );
+        jumpAction = InputSystem.actions.FindAction( "Jump" );
+        rb         = GetComponent<Rigidbody2D>();
     }
-
-    // void MovePlayer(InputAction.CallbackContext context)
-    // {
-    //     // controls
-    //     moveAmount = context.ReadValue<Vector2>();
-
-    //     // animation
-    //     // todo
-
-    //     // player direction
-    //     if( moveAmount.x < 0.0f && ( facingRight == false ) )
-    //         FlipPlayer();
-    //     else if( moveAmount.x > 0.0f && ( facingRight == true ) )
-    //         FlipPlayer();
-
-    //     // physics
-    //     rb.linearVelocity = new Vector2 ( moveAmount.x * playerSpeed ,
-    //                                       rb.linearVelocity.y );
-
-    // }
 
     void JumpPlayer()
     {
-        rb.AddForceAtPosition(new Vector2(0, jumpSpeed), Vector2.up, ForceMode2D.Impulse );
+        rb.AddForceAtPosition( new Vector2( 0, jumpSpeed ), Vector2.up, ForceMode2D.Impulse );
     }
 
     // Update is called once per frame
@@ -67,8 +47,6 @@ public class PlayerMovement : MonoBehaviour
                                           rb.linearVelocity.y );
 
         if( jumpAction.WasPressedThisFrame() )
-        {
             JumpPlayer();
-        }
     }
 }
